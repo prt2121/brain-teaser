7ac3ac6ec15138fa90615bcbcadad0573b4a62fd
Prat <banqfirn@gmail.com>
2015-10-02 21:01:17 -0400

    Add binarySearch

--- 2sum01/src/Main.hs
+++ 2sum01/src/Main.hs
@@ -1,14 +1,29 @@
 module Main where
 
 import Data.List
+import Data.Array
 -- import Control.Monad
 -- import qualified Data.IntMap as IntMap
 
 twoSum :: [Integer] -> Integer -> Maybe (Integer, Integer)
-twoSum [] t     = Nothing
+twoSum [] _     = Nothing
 twoSum (l:ls) t = case (find (\x -> x + l == t) ls) of
                     Just y -> Just (l, y)
                     Nothing -> twoSum ls t
+
+-- *Main> let arr = array (1,3) [(1,42),(2,87),(3,95)]
+-- *Main> binarySearch  arr 87 1 3
+-- Just 2
+binarySearch :: (Integral a, Ord a1, Ix a) => Array a a1 -> a1 -> a -> a -> Maybe a
+binarySearch haystack needle l h
+  | l > h     = Nothing
+  | pivot > needle = binarySearch haystack needle l (m-1)
+  | pivot < needle = binarySearch haystack needle (m+1) h
+  | otherwise = Just m
+  where
+    m = l + (h-l) `div` 2
+    pivot = haystack!m
+
 
 main :: IO ()
 main = do
