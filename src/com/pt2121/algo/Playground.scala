f5de347b8b2a0018238131f5c198d85d2d21e8ae
Prat <banqfirn@gmail.com>
2015-10-04 16:09:24 -0400

    Add In-place reverse

--- src/com/pt2121/algo/Playground.scala
+++ src/com/pt2121/algo/Playground.scala
@@ -19,8 +19,37 @@ object Playground {
     result trim
   }
 
+  // imperative
+  // 'In-place' reverse
+  def reverseWords2(s: Array[Char]): String = {
+    reverse(s, 0, s.size - 1)
+    var i = 0
+    for(j <- 0 to s.size) {
+      if (j == s.length || s(j) == ' ') {
+         reverse(s, i, j - 1)
+         i = j + 1
+      }
+    }
+    s.mkString
+  }                                         //> reverseWords2: (s: Array[Char])String
+
+  // imperative
+  // reverse string 'in place'
+  def reverse(s: Array[Char], begin: Int, end: Int) {
+  var b = begin; var e = end
+    while(b < e) {
+      val tmp = s(b)
+      s(b) = s(e)
+      s(e) = tmp
+      b = b + 1
+      e = e - 1
+    }
+  }
+
   def main(args: Array[String]) {
     reverseWords(" a ")
-    reverseWords(" abc xyz 1234 ") 
+    reverseWords(" abc xyz 1234 ")
+    var test = "this is a test "
+    println(reverseWords2(test.toCharArray))
   }
 }
