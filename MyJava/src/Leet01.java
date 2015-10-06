e43f6bb52f869a49fe3d06440f5fdef25c99d3f5
Prat <banqfirn@gmail.com>
2015-10-05 22:40:15 -0400

    Add lengthOfLongestSubstring 2, 3

--- MyJava/src/Leet01.java
+++ MyJava/src/Leet01.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by pt2121 on 10/5/15.
  */
@@ -84,13 +86,42 @@ public class Leet01 {
         return exist;
     }
 
+    public static int lengthOfLongestSubstring2(String s) {
+        boolean[] exist = new boolean[256];
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            while (exist[s.charAt(j)]) {
+                exist[s.charAt(i)] = false;
+                i++;
+            }
+            exist[s.charAt(j)] = true;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
+    }
+
+    public static int lengthOfLongestSubstring3(String s) {
+        int[] charMap = new int[256];
+        Arrays.fill(charMap, -1);
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (charMap[s.charAt(j)] >= i) {
+                i = charMap[s.charAt(j)] + 1;
+            }
+            charMap[s.charAt(j)] = j;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
 //        try {
 //            System.out.println(atoi("2147483648"));
 //        } catch (Exception e) {
 //            System.out.println(e.getMessage());
 //        }
-        int l = lengthOfLongestSubstring("bbbbb");
+        int l = lengthOfLongestSubstring3("abcdcedf");
+
         System.out.println(l);
     }
 }
