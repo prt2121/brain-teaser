396835f8352d0b787e0548cfafc0db2a36c25768
Prat <banqfirn@gmail.com>
2015-11-07 12:03:53 -0500

    Solve problem 2

--- euler-hs/src/Main.hs
+++ euler-hs/src/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-euler1 = sum [n | n <- [0..999], (mod n 3) == 0 || (mod n 5) == 0]
+e1 = sum [n | n <- [0..999], (mod n 3) == 0 || (mod n 5) == 0]
+
+fib :: [Integer]
+fib = f 0 1
+        where f a b = a : f b (a + b)
+
+e2 :: Integer
+e2 = sum(takeWhile (<4000000) [n | n <- fib, n `mod` 2 == 0])
+
+fib' :: Integer -> Integer
+fib' 0 = 1
+fib' 1 = 1
+fib' n = fib'(n - 1) + fib'(n -2)
+
+fibs' :: [Integer]
+fibs' = 1 : 1 : zipWith (+) fibs' (tail fibs')
 
 main :: IO ()
 main = do
