99fca755d80065ad5cf468638747b3d144817986
Prat <banqfirn@gmail.com>
2015-11-13 23:29:24 -0500

    Move parser to new project

--- euler-hs/src/Main.hs
+++ euler-hs/src/Main.hs
@@ -2,39 +2,6 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
-data LispVal =  Atom String
-              | List [LispVal]
-              | DottedList [LispVal] LispVal
-              | Number Integer
-              | String String
-              | Bool Bool
-
-symbol :: Parser Char
-symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-parseString :: Parser LispVal
-parseString = do char '"'
-                  x <- many (noneOf "\"")
-                  char '"'
-                  return $ String x
-
-parseAtom :: Parser LispVal
-parseAtom = do  first <- letter <|> symbol
-                rest <- many (letter <|> digit <|> symbol)
-                let atom = [first] ++ rest
-                return $ case atom of
-                          "#t" -> Bool True
-                          "#f" -> Bool False
-                          otherwise -> Atom atom
-
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value!"
-
 e1 = sum [n | n <- [0..999], (mod n 3) == 0 || (mod n 5) == 0]
 
 fib :: [Integer]
@@ -54,6 +21,4 @@ fibs' = 1 : 1 : zipWith (+) fibs' (tail fibs')
 
 main :: IO ()
 main = do
-        putStrLn ("insert your string")
-        str <- getLine
-        putStrLn (readExpr str)
+  putStrLn ("insert your string")
---
+++ write-yourself-scheme/src/Main.hs
@@ -0,0 +1,41 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal =  Atom String
+              | List [LispVal]
+              | DottedList [LispVal] LispVal
+              | Number Integer
+              | String String
+              | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do  char '"'
+                  x <- many (noneOf "\"")
+                  char '"'
+                  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do  first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol)
+                let atom = [first] ++ rest
+                return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value!"
+
+main :: IO ()
+main = do
+  putStrLn ("insert your string")
+  str <- getLine
+  putStrLn (readExpr str)
