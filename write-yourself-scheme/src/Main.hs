ddaaa44eab1a1ccb8b54f6b8220a4b3ac078225c
Prat <banqfirn@gmail.com>
2015-11-18 21:52:42 -0500

    Add parseNumber

--- write-yourself-scheme/src/Main.hs
+++ write-yourself-scheme/src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 
 data LispVal =  Atom String
               | List [LispVal]
@@ -14,8 +15,13 @@ symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+-- class Monad m => Stream s m t | s -> t where
+-- An instance of Stream has stream type s, underlying monad m and token type t determined by the stream
+
+-- ParsecT s u m a is a parser with stream type s, user state type u, underlying monad m and return type a.
+
 parseString :: Parser LispVal
-parseString = do  char '"'
+parseString = do  char '"' -- Stream s m Char => ParsecT s u m ()
                   x <- many (noneOf "\"")
                   char '"'
                   return $ String x
@@ -28,6 +34,12 @@ parseAtom = do  first <- letter <|> symbol
                           "#t" -> Bool True
                           "#f" -> Bool False
                           otherwise -> Atom atom
+
+-- liftM :: Monad m => (a1 -> r) -> m a1 -> m r
+-- fmap :: Functor f => (a -> b) -> f a -> f b
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
 
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
