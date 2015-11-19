2a89f34003340b1717d559f0c1ee30194709defb
Prat <banqfirn@gmail.com>
2015-11-18 21:49:02 -0500

    Add bigPrime

--- euler-hs/src/Main.hs
+++ euler-hs/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 
 e1 = sum [n | n <- [0..999], (mod n 3) == 0 || (mod n 5) == 0]
 
@@ -18,6 +19,20 @@ fib' n = fib'(n - 1) + fib'(n -2)
 
 fibs' :: [Integer]
 fibs' = 1 : 1 : zipWith (+) fibs' (tail fibs')
+
+bigPrime :: Integer -> Integer
+bigPrime n =
+  if isPrime n
+    then n
+    else maximum x
+        where x = do
+              i <- [2 .. n]
+              guard (n `mod` i == 0 && isPrime i)
+              return i
+
+isPrime x = not $ any divisible $ takeWhile notTooBig [2..] where
+              divisible y = x `mod`y == 0
+              notTooBig y = y*y <= x
 
 main :: IO ()
 main = do
