9525bd4adf42273432ab4920bd1178df31745831
Prat <banqfirn@gmail.com>
2015-11-20 15:54:20 -0500

    Optimize LongestSubList

--- LeetJava/src/LongestSubList.java
+++ LeetJava/src/LongestSubList.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -9,6 +11,21 @@ import java.util.stream.Collectors;
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 public class LongestSubList {
+
+  public static <T> int lengthOfLongestSubList2(List<T> list) {
+    Map<T, Integer> positions = new HashMap<>();
+    int maxLen = 0;
+    int left = 0;
+    for(int i = 0; i < list.size(); i++) {
+      T current = list.get(i);
+      if(positions.containsKey(current) && positions.get(current) >= left) {
+        left = positions.get(current) + 1;
+      }
+      positions.put(current, i);
+      maxLen = Math.max(i - left + 1, maxLen);
+    }
+    return maxLen;
+  }
 
   public static <T> int lengthOfLongestSubList(List<T> list) {
     Set<T> set = new HashSet<>();
@@ -39,6 +56,6 @@ public class LongestSubList {
 
   public static void main(String[] args) {
     List<Character> chars = "abcabcbbadbc".chars().mapToObj(e -> (char) e).collect(Collectors.toList());
-    System.out.println(LongestSubList.lengthOfLongestSubList(chars));
+    System.out.println(LongestSubList.lengthOfLongestSubList2(chars));
   }
 }
