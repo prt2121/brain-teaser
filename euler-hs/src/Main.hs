3fb636e2ccc8e359478d42c08ca4f365b7cff257
Prat <banqfirn@gmail.com>
2015-11-20 11:09:10 -0500

    Add Smallest multiple

--- euler-hs/src/Main.hs
+++ euler-hs/src/Main.hs
@@ -22,8 +22,8 @@ fibs' :: [Integer]
 fibs' = 1 : 1 : zipWith (+) fibs' (tail fibs')
 
 -- freq [5,7,7,1,2,4,4,4] = [(1,1),(2,1),(4,3),(5,1),(7,2)]
-freq :: Ord a => [a] -> [(a, Int)]
-freq = map (\x -> (head x, length x)) . group . sort
+freq :: Ord a => [a] -> [(a, Integer)]
+freq = map (\x -> (head x, toInteger(length x))) . group . sort
 
 -- maxSecond [(7,1),(2,1),(4,3),(4,1),(7,2)] = [(2,1),(4,3),(7,2)]
 maxSecond ls = foldr f [] ls
@@ -34,11 +34,22 @@ maxSecond ls = foldr f [] ls
                                         else acc
                                     else x : acc
 
+-- allPrimeFactors :: Integral t => t -> [t]
+allPrimeFactors :: Integer -> [Integer]
 allPrimeFactors n =
   case factors of
     [] -> [n]
     _  -> factors ++ allPrimeFactors (n `div` (head factors))
   where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
+
+f :: [Integer] -> [(Integer, Integer)]
+f = maxSecond . (freq . allPrimeFactors =<<)
+-- f ls = maxSecond(concat(map (freq . allPrimeFactors) ls))
+
+e5 :: [Integer] -> Integer
+e5 = product . (map (\x -> (fst x) ^ (snd x))) . f
+
+-- h = map (\x -> (fst x) ^ (snd x))
 
 -- common prime factors
 cpf :: Integer -> [Integer]
