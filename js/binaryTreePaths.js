451d63d9824d7587f0514ea4a427910b2fc34501
Prat <banqfirn@gmail.com>
2015-11-24 10:42:47 -0500

    Add BFS

--- js/binaryTreePaths.js
+++ js/binaryTreePaths.js
@@ -1,13 +1,13 @@
 function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
+  this.visited = false;
 }
 
 var result = [];
 
 /**
  * @param {TreeNode} root
- * @return {string[]}
  */
 var binaryTreePaths = function(root) {
   dfs(root)
@@ -21,6 +21,28 @@ var dfs = function(node) {
   if (node.right !== null) {
     dfs(node.right);
   }
+}
+
+var bfs = function(node) {
+  var ret = [];
+  var queue = [];
+  queue.push(node);
+  ret.push(node.val);
+  node.visited = true
+  while(queue.length > 0) {
+    var n = queue.shift()
+    if (n.left !== null && !n.left.visited) {
+      n.left.visited = true;
+      queue.push(n.left);
+      ret.push(n.left.val);
+    }
+    if (n.right !== null && !n.right.visited) {
+      n.right.visited = true;
+      queue.push(n.right);
+      ret.push(n.right.val);
+    }
+  }
+  return ret;
 }
 
 var zero = new TreeNode(0)
@@ -39,6 +61,6 @@ five.left = four
 five.right = seven
 seven.left = six
 
-new binaryTreePaths(three);
+// new dfs(three);
 
-console.log(result)
+console.log(new bfs(three))
