2ef89bde8b29fb31b2643cf390c6cd82849fdd02
Prat <banqfirn@gmail.com>
2015-12-04 17:06:38 -0500

    Move TreeNode to a separate class

---
+++ leet/src/main/kotlin/TreeNode.kt
@@ -0,0 +1,3 @@
+data class TreeNode(val value: Int, val left: TreeNode?, val right: TreeNode?) {
+  constructor(value: Int) : this(value, null, null)
+}
--- leet/src/main/kotlin/kthSmallestBst.kt
+++ leet/src/main/kotlin/kthSmallestBst.kt
@@ -43,6 +43,3 @@ fun toOrderedList(node: TreeNode?): List<Int> {
   }
 }
 
-data class TreeNode(val value: Int, val left: TreeNode?, val right: TreeNode?) {
-  constructor(value: Int) : this(value, null, null)
-}
