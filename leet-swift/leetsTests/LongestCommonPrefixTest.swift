dd8ba01f5dbcc44b736875b03f492ce95083f261
Prat <banqfirn@gmail.com>
2016-01-01 13:33:09 -0500

    Add more tests to LongestCommonPrefixTest

--- leet-swift/leetsTests/LongestCommonPrefixTest.swift
+++ leet-swift/leetsTests/LongestCommonPrefixTest.swift
@@ -35,4 +35,51 @@ class LongestCommonPrefixTest: XCTestCase {
         assert(expected == prefix)
     }
     
+    func testCase005() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["a"])
+        let expected = "a"
+        assert(expected == prefix)
+    }
+    
+    func testCase006() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["a", "b"])
+        let expected = ""
+        assert(expected == prefix)
+    }
+    
+    func testCase007() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["a", "ab"])
+        let expected = "a"
+        assert(expected == prefix)
+    }
+    
+    func testCase008() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["a", "ab", ""])
+        let expected = ""
+        assert(expected == prefix)
+    }
+    
+    func testCase009() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["a", "ab", "c"])
+        let expected = ""
+        assert(expected == prefix)
+    }
+    
+    func testCase010() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["ab", "ab", "ab"])
+        let expected = "ab"
+        assert(expected == prefix)
+    }
+    
+    func testCase011() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["abc", "ab", "ab"])
+        let expected = "ab"
+        assert(expected == prefix)
+    }
+    
+    func testCase012() {
+        let prefix : String = LongestCommonPrefix.longestCommonPrefix01(["ab", "ab", "abc"])
+        let expected = "ab"
+        assert(expected == prefix)
+    }
 }
